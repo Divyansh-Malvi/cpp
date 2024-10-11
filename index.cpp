7912d23857963e10315f78c5c232fd7d1e9fa27d
Divyansh Malvi <divymalvi2005@gmail.com>
2024-10-11 16:34:22 +0530

    first commit

---
+++ index.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+
+
+{
+ int a,b;
+/*cout<<"enter 1 value" ;
+cin>>a;
+cout<<"enter 2 value";
+cin>>b;*/
+//cout<<"enter 2 number\n";
+//cin>>a>>b;
+a=12;
+{
+    int a=90;
+    cout<<::a;
+}
+
+}
+
+
